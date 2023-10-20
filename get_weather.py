6906e0611e6899aaac09adcb3b81dc95a23cab9c
w0475858 <144358689+w0475858@users.noreply.github.com>
2023-10-20 09:55:17 -0300

    new changes

--- get_weather.py
+++ get_weather.py
@@ -1,1 +1,20 @@
 
+import requests
+
+city = "Halifax, Nova Scotia"
+
+searched_results = f"https://geocode.maps.co/search?q={city}"
+
+geocode_info = requests.get(searched_results)
+
+json_resonse = geocode_info.json()
+
+
+lat = json_resonse["lat"]
+lon = json_resonse["lon"]
+
+weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,windspeed_10m&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+
+weather_data = requests.get(weather_url)
+
+print(weather_data)
